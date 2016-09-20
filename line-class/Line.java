29a0efda3cbdc436c6070c4618549951e1417c2a
Tommy Dayton <Tommy Dayton>
2016-09-20 12:05:30 -0400

    REALLY Finished Line Generator

--- line-class/Line.java
+++ line-class/Line.java
@@ -10,10 +10,24 @@ public class Line
     // Defines the equation for the line.
     private String lineEquation;
     // Inputs the slope and y-intercept of the line
-    public Line(double slope, double yIntercept){
+    /**
+     * Description: Defines the slope and y-intercept, and creates the line.
+     * Input: double slope, double yIntercept
+     * Output: void
+     * Purpose: To create the line.
+     * Example: 3.5 9
+     */
+    public void Line(double slope, double yIntercept){
         lineEquation = "y = " + slope + "x + " + yIntercept;
     }
     // Prints out the line.
+    /**
+     * Description: Prints out the line created.
+     * Input: none
+     * Output: void
+     * Purpose: To show the user the line.
+     * Example: y = 3.5x + 9
+     */
     public void getLineEquation(){
         System.out.println(lineEquation);
     }
